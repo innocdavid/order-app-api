0561de1e418058715cd4f37d0fb69c8dbabc9f3a
EmmanuelJessi <107475789+EmmanuelJessi@users.noreply.github.com>
2023-03-29 22:26:22 +0300

    add itemUrl image and reviews

--- orders.js
+++ orders.js
@@ -23,37 +23,37 @@ export default [
     {
         itemName: "Sandwich",
         price: 10.00,
-        itemUrl: "",
+        itemUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109592/OrderApp/sandwich_d98te8.jpg",
         rating: 3,
         reviews: ["", ""]
     },
     {
         itemName: "French Fries",
         price: 5.00,
-        itemUrl: "",
+        itemUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109590/OrderApp/fries_h2alxr.jpg",
         rating: 4.6,
         reviews: ["Great taste", "Enjoyed the dish"] 
     },
     {
         itemName: "Fruit Salad",
         price: 10.00,
-        itemUrl: "",
+        itemUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109590/OrderApp/fruits-salad_j50utt.jpg",
         rating: 4.0,
-        reviews: ["", "", ""]
+        reviews: ["Fantastic", "Test Great", "WOW Nice"]
     },
     {
         itemName: "Pepperoni Pizza",
         price: 15.00,
-        itemUrl: "",
+        itemUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680117515/OrderApp/pepperoni-pizza_opskbc.jpg",
         rating: 4.6,
-        reviews: ["", "", ""]
+        reviews: ["Good", "very Good", "Excellent"]
     },
     {
         itemName: "Straw Berries", 
         price: 3.00,
-        itemUrl: "",
+        itemUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109588/OrderApp/drinks_qf25qm.jpg",
         rating: 4.1,
-        reviews: ["", "", ""]
+        reviews: ["Fair", "Good", "Test Nice"]
     }
 
 ]
