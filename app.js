6094d82eea337e58220bc9b22f959d8491adf9b0
Banja David Lotigo <innocdavid@gmail.com>
2023-04-16 20:11:57 +0300

    Call menuRouter on app file

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 import express from "express";
-import orders from "./orders.js"
+import menuRoute from "./routes/menuRouter.js";
+
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json(orders)
-});
+app.use('/api/menu', menuRoute);
 
 app.listen(8080, () => {
     console.log("server listening from port 8080");
