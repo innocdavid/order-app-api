914c849c4d68b49d57a87f617a7c12b514227203
Banja David Lotigo <innocdavid@gmail.com>
2023-04-16 20:43:26 +0300

    Add response status

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -3,7 +3,11 @@ import asyncHandler from "express-async-handler";
 import orders from "../orders.js";
 
 const getMenu = asyncHandler(async (req, res) => {
-  res.json(orders);
+  res.status(200)
+    .json({
+      orders: orders,
+      message: "orders returned successfully"
+    })
 })
 
 export default getMenu;
