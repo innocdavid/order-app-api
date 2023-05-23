60644f42fc995f3412f6908fda408fa6d9865ee4
Banja David Lotigo <innocdavid@gmail.com>
2023-05-23 10:20:34 +0300

    Add swagger documentation for the get cover image api

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'.bgBlue));
 
 app.use('/api/items', menuRoute);
-app.use('/api/cover-image', menuRoute);
+app.use('/api/cover-images', coverImageRoute);
 
 if (process.env.NODE_ENV !== 'test') {
     // server listening
--- routes/coverImageRoute.js
+++ routes/coverImageRoute.js
@@ -6,7 +6,44 @@ import express from 'express';
 
 const router = express.Router();
 
-
+/**
+ * @openapi
+ * '/api/cover-images':
+ *  get:
+ *     tags:
+ *     - Cover Images  
+ *     summary: Get the list of cover images
+ *     description: Returns the list of available images with their id, name and url.
+ *     responses:
+ *       200:
+ *         description: A list of cover images
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              images:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: number
+ *                    description: The id of the image
+ *                  name:
+ *                    type: string
+ *                    description: The name of the image
+ *                  url:
+ *                    type: number
+ *                    description: The url of the image 
+ *          400:
+ *            description: 'Image not found'
+ *            schema: 
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *            example:
+ *              application/json: 
+ *                error: 'image not found'
+ */
 router.get("/", fetchAllCoverImages);
 
 
