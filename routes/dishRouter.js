b3e3a18428373f777a64c7522accca32ed546913
Banja David Lotigo <innocdavid@gmail.com>
2023-06-01 16:27:29 +0300

    Add api documentation for a get dish by id route

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -71,6 +71,85 @@ const router = express.Router()
  *                error: 'dish not found'
  */
 router.get("/", fetchAllDishes);
+
+/**
+ * @openapi
+ * '/api/dishes/{id}':
+ *  get:
+ *     tags:
+ *      - Dishes 
+ *     summary: Get a single cover dish
+ *     description: Get an dish by its id
+ *     parameters:
+ *      - name: id
+ *        in: path
+ *        description: dish id
+ *        required: true
+ *        type: integer  
+ *     responses:
+ *       200:
+ *        description: dish returned
+ *        content:
+ *          application/json:  
+ *            schema:
+ *              type: array
+ *              dishe:
+ *                type: object
+ *                properties:
+ *                  user:
+ *                    type: number
+ *                    description: The id of a user
+ *                  name:
+ *                    type: string
+ *                    description: The name of a dish
+ *                  price:
+ *                    type: number
+ *                    description: The price of a dish 
+ *                  category:
+ *                    type: String
+ *                    description: The category a the dish  
+ *                  cookingDuration:
+ *                    type: string
+ *                    description: The duration a the dish
+ *                  sizes:
+ *                    type: Array
+ *                    description: The sizes of a dish
+ *                  imageUrl:
+ *                    type: String
+ *                    description: The image-url of a dish
+ *                  ingredients:
+ *                    type: Array
+ *                    description: The ingredients a the dish
+ *                  nuitrients:
+ *                    type: Array
+ *                    description: The nuitrients a the dish
+ *                  rating:
+ *                    type: number
+ *                    description: The rating of a dish
+ *                  reviews: 
+ *                    type: Array
+ *                    description: the reviews of a dish
+ *        400:
+ *          description: 'Cover image not found'
+ *          schema: 
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ *              example:
+ *                application/json: 
+ *                  error: 'Bad request'
+ *          404:
+ *            description: 'Cover image not found'
+ *            schema: 
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  example:
+ *                    application/json: 
+ *                      error: 'Item not found'
+ */
 router.get("/:id", fetchSingleDish);
 
 export default router;
