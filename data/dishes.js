2daabcc790a69d53409e589aea3c3ea84e00e594
Banja David Lotigo <innocdavid@gmail.com>
2023-06-01 16:00:39 +0300

    Update dishes file

---
+++ data/dishes.js
@@ -0,0 +1,110 @@
+export default [
+    {   
+        name: "Grilled Meat",
+        price: 20.00,
+        decription: "lorem ipsum dolor sit amet, consect",
+        category: "Meat",
+        cookingDuration: "2 hours",
+        sizes: ["Large", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109590/OrderApp/grilled-meat_xkyxuf.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4,
+        reviews: [] 
+    },
+    {
+        name: "Hamburger",
+        price: 22.00,
+        decription: "lorem Ipsum is Lorem Ipsum",
+        category: "Meat",
+        cookingDuration: "2 hours",
+        sizes: ["Large", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680111009/OrderApp/burger_ivbbyi.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4.5,
+        reviews: [] 
+    },
+    {
+        name: "Shrimps",
+        price: 15.00,
+        decription: "lorem ipsum dolor sit amet, consectetur adip",
+        category: "Drinks",
+        cookingDuration: "1 hour",
+        sizes: ["Larger", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109591/OrderApp/shrimps_unnlnw.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4.3,
+        reviews: [] 
+    },
+    {
+        name: "Sandwich",
+        price: 10.00,
+        decription: "lorem ipsum dolor sit amet",
+        category: "Desert",
+        cookingDuration: "30 minutes",
+        sizes: ["Larger", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109592/OrderApp/sandwich_d98te8.jpg",
+        ingradients: [ ],
+        nuitrients: [],
+        rating: 3,
+        reviews: []
+    },
+    {
+        name: "French Fries",
+        price: 5.00,
+        decription: "lorem ipsum dolor sit amet, consectetur",
+        category: "Fries",
+        cookingDuration: "40 minutes",
+        sizes: ["Larger", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109590/OrderApp/fries_h2alxr.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4.6,
+        reviews: [] 
+    },
+    {
+        name: "Fruit Salad",
+        price: 10.00,
+        decription: "lorem Ipsum",
+        category: "Fruits",
+        cookingDuration: "2 minutes",
+        sizes: ["Larger", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109590/OrderApp/fruits-salad_j50utt.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4.0,
+        reviews: []
+    },
+    {
+        name: "Pepperoni Pizza",
+        price: 15.00,
+        decription: "lorem ipsum dolor sit amet",
+        category: "Bread",
+        cookingDuration: "50 minutes",
+        sizes: ["Larger", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680117515/OrderApp/pepperoni-pizza_opskbc.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4.6,
+        reviews: []
+    },
+    {
+        name: "Straw Berries", 
+        price: 3.00,
+        decription: "lorem ipsum dolor sit amet, consect",
+        category: "Fruites",
+        cookingDuration: "2 minutes",
+        sizes: ["Larger", "Small", "Medium"],
+        imageUrl: "https://res.cloudinary.com/ddgryadig/image/upload/v1680109588/OrderApp/drinks_qf25qm.jpg",
+        ingradients: [],
+        nuitrients: [],
+        rating: 4.1,
+        reviews: []
+    }
+
+]
+     
+
+
