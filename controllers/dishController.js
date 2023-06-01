55e0a9d6ace56b77142344eee6236f3efced95a1
Banja David Lotigo <innocdavid@gmail.com>
2023-06-01 16:26:38 +0300

    Refine code

--- controllers/dishController.js
+++ controllers/dishController.js
@@ -28,9 +28,9 @@ const fetchSingleDish = asyncHandler(async (req, res) => {
 
     const dishModel = await DishModel.findById(id);
     if (!dishModel) {
-      return res.status(404).json({ message: `Dish with ${id} not found` });
+      return res.status(404).json({ message: `Dish with id ${id} not found` });
     }
-    res.status(200).json({ dishModel, message: `Dish with ${id} returned successfully` });
+    res.status(200).json({ dishModel, message: `Dish with id ${id} returned successfully` });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
