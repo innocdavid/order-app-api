997c21900a92cb2794de55d85dff0a7052bd1b88
Banja David Lotigo <innocdavid@gmail.com>
2023-06-02 09:54:53 +0300

    Add category router to app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import cors from "cors";
 import colors from "colors";
 import morgan from "morgan";
+import categoryRoute from "./routes/categoryRoute.js";
 import dishRoute from "./routes/dishRouter.js";
 import coverImageRoute from "./routes/coverImageRoute.js";
 import swaggerDocs from "./config/swagger.js"
@@ -19,12 +20,13 @@ dbConnection()
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'.bgBlue));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms".bgBlue));
 
-app.use('/api/dishes', dishRoute);
-app.use('/api/cover-images', coverImageRoute);
+app.use("/api/dishes", dishRoute);
+app.use("/api/cover-images", coverImageRoute);
+app.use("/api/categories", categoryRoute);
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
     // server listening
     app.listen(process.env.PORT, () => {
         console.log(`server listening from ${process.env.HOST}:${process.env.PORT}`.bgYellow);
