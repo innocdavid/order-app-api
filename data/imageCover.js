fac8e0270050b3e797fd270172ad1a0dbe8157b7
Banja David Lotigo <innocdavid@gmail.com>
2023-06-02 12:39:44 +0300

    Update image cover url

--- data/imageCover.js
+++ data/imageCover.js
@@ -1,9 +1,10 @@
 const coverImages = [
-  { id: 0, name: 'image0', url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-psd%2Famazing-promotion-fast-food-social-media-post-template_26245925.htm&psig=AOvVaw09PwFOysXcwILCdmSYc1V7&ust=1684910289101000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKiDnI3qiv8CFQAAAAAdAAAAABAE' },
-  { id: 1, name: 'image1', url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.postermywall.com%2Findex.php%2Fart%2Ftemplate%2F35bb0660e8d9869292396db67abda272%2Ffast-food-restaurant-promotion-instagram-post-design-template&psig=AOvVaw09PwFOysXcwILCdmSYc1V7&ust=1684910289101000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKiDnI3qiv8CFQAAAAAdAAAAABAI' },
-  { id: 2, name: 'image2', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQedEglChoLr5-i4AuEvoibOdfvm54Ruhnuk8sqNh8E&s' },
-  { id: 3, name: 'image3', url: 'https://cdn4.vectorstock.com/i/1000x1000/10/88/fast-food-restaurant-social-media-advertising-vector-31181088.jpg' },
-  { id: 4, name: 'image4', url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Ffood-promotion&psig=AOvVaw09PwFOysXcwILCdmSYc1V7&ust=1684910289101000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKiDnI3qiv8CFQAAAAAdAAAAABAi' },
+  { id: 0, name: 'image0', url: 'https://res.cloudinary.com/dknnf52ij/image/upload/v1685696650/fast-food/cover-images/Screenshot_2023-06-02_at_11.53.32_AM_xwnvas.png' },
+  { id: 1, name: 'image1', url: 'https://res.cloudinary.com/dknnf52ij/image/upload/v1685696645/fast-food/cover-images/Screenshot_2023-06-02_at_11.56.31_AM_texlq8.png' },
+  { id: 2, name: 'image2', url: 'https://res.cloudinary.com/dknnf52ij/image/upload/v1685696645/fast-food/cover-images/Screenshot_2023-06-02_at_11.55.26_AM_vnyfy1.png' },
+  { id: 3, name: 'image3', url: 'https://res.cloudinary.com/dknnf52ij/image/upload/v1685696644/fast-food/cover-images/Screenshot_2023-06-02_at_11.56.13_AM_tau5qq.png' },
+  { id: 4, name: 'image4', url: 'https://res.cloudinary.com/dknnf52ij/image/upload/v1685696644/fast-food/cover-images/Screenshot_2023-06-02_at_11.54.36_AM_vbbdjc.png' },
+  { id: 5, name: 'image4', url: 'https://res.cloudinary.com/dknnf52ij/image/upload/v1685696644/fast-food/cover-images/Screenshot_2023-06-02_at_11.53.51_AM_uc7r25.png' },
 ]
 
 export default coverImages;
