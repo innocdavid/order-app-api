05251a01f3f31e1e897377c01b6a22b6748a6ed4
Banja David Lotigo <innocdavid@gmail.com>
2023-06-02 09:21:37 +0300

    Add a data file for categories

---
+++ data/categories.js
@@ -0,0 +1,107 @@
+export default [ 
+  {
+    id: 1,
+    name: "Hamburger",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684930/fast-food/categories/Screenshot_2023-06-02_at_8.22.00_AM_bqoxll.png",
+  },
+  {
+    id: 2,
+    name: "Cheeseburger",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684930/fast-food/categories/Screenshot_2023-06-02_at_8.23.08_AM_wt2kes.png",
+  },
+  {
+    id: 3,
+    name: "Sandwich",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684930/fast-food/categories/Screenshot_2023-06-02_at_8.23.37_AM_enam1w.png",
+  },
+  {
+    id: 4,
+    name: "Milk shake",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684930/fast-food/categories/Screenshot_2023-06-02_at_8.24.29_AM_akmzqf.png",
+  },
+  {
+    id: 5,
+    name: "Muffin",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.25.26_AM_bepnj2.png",
+  },
+  {
+    id: 6,
+    name: "Burrito",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684930/fast-food/categories/Screenshot_2023-06-02_at_8.26.02_AM_gokgrq.png",
+  },
+  {
+    id: 7,
+    name: "Cookie",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.27.08_AM_j1zy4h.png",
+  },
+  {
+    id: 8,
+    name: "Taco",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.27.49_AM_alkfnw.png",
+  },
+  {
+    id: 9,
+    name: "Hot dog",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.28.27_AM_ytnvzi.png",
+  },
+  {
+    id: 10,
+    name: "Fried chicken",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.30.37_AM_vwjakh.png"
+  },
+  {
+    id: 11,
+    name: "Donuts",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.31.09_AM_ltxznp.png"
+  },
+  {
+    id: 12,
+    name: "Baguette",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.32.12_AM_zddize.png"
+  },
+  {
+    id: 13,
+    name: "Soft drink",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.33.20_AM_wnra2n.png"
+  },
+  {
+    id: 14,
+    name: "Pizza",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.34.24_AM_qtcxel.png"
+  }, 
+  {
+    id: 15,
+    name: "Onion ring",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.35.12_AM_ds2vr1.png"
+  },
+  {
+    id: 16,
+    name: "Sausage",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684931/fast-food/categories/Screenshot_2023-06-02_at_8.36.07_AM_xgyfvu.png"
+  },
+  {
+    id: 17,
+    name: "Pretzel",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.37.20_AM_cnfdqw.png"
+  },
+  {
+    id: 18,
+    name: "Pancake",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.38.02_AM_ltcmrw.png"
+  },
+  {
+    id: 19,
+    name: "Bacon",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.38.44_AM_j3ushk.png"
+  },
+  {
+    id: 20,
+    name: "Chips",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.40.06_AM_i6ydfb.png"
+  },
+  {
+    id: 21,
+    name: "Noodle",
+    imageUrl: "https://res.cloudinary.com/dknnf52ij/image/upload/v1685684932/fast-food/categories/Screenshot_2023-06-02_at_8.40.40_AM_dzu7c9.png"
+  }
+]
