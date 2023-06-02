863653f719f719d63726a85a7b660356d23f7a4e
Banja David Lotigo <innocdavid@gmail.com>
2023-06-02 09:54:24 +0300

    Add route for fetch list of available categories and api documentation

---
+++ routes/categoryRoute.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import {
+  fetchAllCategories
+} from "../controllers/categoryController.js";
+
+const router = express.Router();
+
+/**
+ * @openapi
+ * '/api/categories':
+ *  get:
+ *     tags:
+ *     - Categories  
+ *     summary: Fetch list of available categories
+ *     description: Returns the list of available categories with their id, name and url.
+ *     responses:
+ *       200:
+ *         description: Sucessfully fetched categories
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              categories:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: number
+ *                    description: The id of the category
+ *                  user:
+ *                    type: string
+ *                    description: The name of user
+ *                  name:
+ *                    type: string
+ *                    description: The name of the category
+ *                  url:
+ *                    type: number
+ *                    description: The url of the category
+ *          400:
+ *            description: 'Image not found'
+ *            schema: 
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *            example:
+ *              application/json: 
+ *                error: 'image not found'
+ */
+router.get("/", fetchAllCategories);
+
+export default router;
