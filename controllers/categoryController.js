c3da5dc873a2672e744e48761565ce353ca1d338
Banja David Lotigo <innocdavid@gmail.com>
2023-06-02 10:06:59 +0300

    Add api controller to fetch a single category by their id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -15,4 +15,20 @@ const fetchAllCategories = asyncHandler(async (req, res) => {
   }
 });
 
-export { fetchAllCategories };
+const fetchSingleCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: 'Invalid cover image ID' });
+  }
+
+  try {
+    const category = await Category.findById(id);
+    if (!category) return res.status(404).json({ message: `Category with id ${id} not found` });
+    res.status(200).json({ category, message: `Category with id ${id} returned successfully` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export { fetchAllCategories, fetchSingleCategory };
